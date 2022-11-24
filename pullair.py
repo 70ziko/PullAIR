2d84354c9a0c34f5ae34bd9681e4ab1363842127
70ziko <github@realchicken.33mail.com>
2022-11-24 23:32:04 +0100

    first commit

---
+++ pullair.py
@@ -0,0 +1,27 @@
+import json
+import certifi
+import pycurl
+from io import BytesIO
+
+# Creating a buffer as the cURL is not allocating a buffer for the network response
+buffer = BytesIO()
+c = pycurl.Curl()
+
+#initializing the request URL
+c.setopt(c.URL, 'https://api.waqi.info/feed/here/?token=d67b0b88305231aad7f5a6cce8e43da5d312411c')
+
+#setting options for cURL transfer  
+c.setopt(c.WRITEDATA, buffer)
+
+#setting the file name holding the certificates
+c.setopt(c.CAINFO, certifi.where())
+
+# perform file transfer
+c.perform()
+
+#Ending the session and freeing the resources
+c.close()
+
+aqicn = buffer.getvalue()
+
+print(aqicn)
